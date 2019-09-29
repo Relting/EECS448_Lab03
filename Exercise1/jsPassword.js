6596ef49ed7d63f4cf8f15edcc79af210e35d56c
Rachel Elting <rachelelting@gmail.com>
2019-09-29 17:39:23 -0500

    Add text box functionality and preliminary validation function.

---
+++ Exercise1/jsPassword.js
@@ -0,0 +1,21 @@
+function inputTextBox1() {
+  var x = document.getElementById("password1").value;
+}
+
+function inputTextBox2() {
+  var y = document.getElementById("password2").value;
+}
+
+
+function checkIfValid() {
+  if ( x.length < 8 || y.length < 8 ) {
+  alert("The password must be at least eight characters long!");
+}
+  else if ( !(x == y) ) {
+  alert("The passwords don't match!");
+}
+  else if ( x == y ) {
+  alert("The password is valid!");
+}
+
+}
