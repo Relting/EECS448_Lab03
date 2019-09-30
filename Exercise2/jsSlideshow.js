372e0ca764902b966f7e8cd1ddbd7959eac53fae
Rachel Elting <rachelelting@gmail.com>
2019-09-29 20:02:34 -0500

    Finish exercise 2.

--- Exercise2/jsSlideshow.js
+++ Exercise2/jsSlideshow.js
@@ -1,14 +1,43 @@
-function previous() {
+function Previous() {
 
-var img = document.getElementByid("myImgId").src;
+var img = document.getElementById("mainPic").src;
 
-if (img == "RTL1.jpg") {
-
-
+if (img.endsWith("RTL1.jpg")) {
+document.getElementById("mainPic").src = "RTL5.jpg";
+}
+else if (img.endsWith("RTL5.jpg")) {
+document.getElementById("mainPic").src = "RTL4.jpg";
+}
+else if (img.endsWith("RTL4.jpg")) {
+document.getElementById("mainPic").src = "RTL3.jpg";
+}
+else if (img.endsWith("RTL3.jpg")) {
+document.getElementById("mainPic").src = "RTL2.jpg";
+}
+else if (img.endsWith("RTL2.jpg")) {
+document.getElementById("mainPic").src = "RTL1.jpg";
 }
 
 }
 
-function next() {
+function Next() {
+
+var img = document.getElementById("mainPic").src;
+
+if (img.endsWith("RTL1.jpg")) {
+document.getElementById("mainPic").src = "RTL2.jpg";
+}
+else if (img.endsWith("RTL5.jpg")) {
+document.getElementById("mainPic").src = "RTL1.jpg";
+}
+else if (img.endsWith("RTL4.jpg")) {
+document.getElementById("mainPic").src = "RTL5.jpg";
+}
+else if (img.endsWith("RTL3.jpg")) {
+document.getElementById("mainPic").src = "RTL4.jpg";
+}
+else if (img.endsWith("RTL2.jpg")) {
+document.getElementById("mainPic").src = "RTL3.jpg";
+}
 
 }
